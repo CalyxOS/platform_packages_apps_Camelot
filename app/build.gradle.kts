3d585274843b992b3c59f4702a1a76c5a14c17bf
Michael Bestas <mkbestas@gmail.com>
2025-02-20 20:42:41 +0200

    Change package name to avoid conflicts
    Change-Id: I6e911cee47f7e53897615357014debbc3a2a713e

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "org.lineageos.camelot"
+        applicationId = "org.calyxos.camelot"
         minSdk = 35
         targetSdk = 36
         versionCode = 1
